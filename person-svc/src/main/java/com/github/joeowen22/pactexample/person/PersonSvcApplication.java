a0b8f18c8a6edf407ec3c44475661802b3b3e586
Joe Owen <joseph_owen@ymail.com>
2022-10-12 10:35:01 +0100

    first commit

---
+++ person-svc/src/main/java/com/github/joeowen22/pactexample/person/PersonSvcApplication.java
@@ -0,0 +1,13 @@
+package com.github.joeowen22.pactexample.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonSvcApplication.class, args);
+	}
+
+}
---
+++ person-svc/src/test/java/com/github/joeowen22/pactexample/person/PersonSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.joeowen22.pactexample.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
